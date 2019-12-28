0955fc8b6560be4c872cb4953d7d8e778cd234d4
Luc Bergeron <psyluche@gmail.com>
2019-12-28 10:07:50 -0500

    Simple update to reflect the new system layout.

--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,6 +1,6 @@
 extern crate specs;
 use specs::prelude::*;
-use super::{Viewshed, Monster, Name, Position};
+use super::{Viewshed, Monster, Name, Position, Map};
 extern crate rltk;
 use rltk::{Point, console};
 
@@ -15,7 +15,7 @@ impl <'a> System<'a> for MonsterAI {
                         WriteStorage<'a, Position>);
 
     fn run(&mut self, data : Self::SystemData) {
-        let (player_pos, viewshed, monster, name) = data;
+        let (map, player_pos, viewshed, monster, name, position) = data;
 
         for (viewshed,_monster, name) in (&viewshed, &monster, &name).join() {
             if viewshed.visible_tiles.contains(&*player_pos) {
